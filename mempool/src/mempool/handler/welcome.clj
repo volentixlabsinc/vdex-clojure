a58a3a7f53805e406e1885aafa6091e10af9d000
staifa <frantisek.stainer@flexiana.com>
2018-04-03 19:59:03 +0200

    WIP CircleCI integration

--- mempool/src/mempool/handler/welcome.clj
+++ mempool/src/mempool/handler/welcome.clj
@@ -1,6 +1,9 @@
 (ns mempool.handler.welcome
   (:require
-   [integrant.core :as ig]))
+    [compojure.core :refer [routes GET]]
+    [integrant.core :as ig]))
 
 (defmethod ig/init-key :mempool.handler/welcome [_ options]
-  (constantly {:status 200 :headers {"Content-Type" "text/plain"} :body "Microservice"}))
+  (routes 
+    (GET "/" [] 
+         {:status 200 :headers {"Content-Type" "text/plain"} :body "Microservice"})))
