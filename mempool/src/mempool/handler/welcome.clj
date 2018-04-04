02ed54777043274446ba3af7b333eef0eb6f3c24
mariusz-jachimowicz-83 <mariusz.jachimowicz.83@gmail.com>
2018-04-04 05:09:46 -0300

    Fix mempool config

--- mempool/src/mempool/handler/welcome.clj
+++ mempool/src/mempool/handler/welcome.clj
@@ -1,6 +1,10 @@
 (ns mempool.handler.welcome
   (:require
+   [compojure.core :refer [routes GET]]
    [integrant.core :as ig]))
 
 (defmethod ig/init-key :mempool.handler/welcome [_ options]
-  (constantly {:status 200 :headers {"Content-Type" "text/plain"} :body "Microservice"}))
+  ;; (constantly {:status 200 :headers {"Content-Type" "text/plain"} :body "Microservice"})
+  (routes 
+   (GET "/" [] 
+        {:status 200 :headers {"Content-Type" "text/plain"} :body "Microservice"})))
